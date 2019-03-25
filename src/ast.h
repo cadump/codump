141b7055d89c5f1d95c5c1068ee710cacc542a4c
Jáchym Tomášek <jachym.tomasek@outlook.com>
2019-03-25 21:14:38 +0100

    Move AST definitions into a header

---
+++ src/ast.h
@@ -0,0 +1,41 @@
+enum {
+      ASSIGN
+} typedef OPERATOR;
+
+enum {
+      INT,
+      FLOAT,
+      STRING,
+      CHAR
+} typedef TYPE;
+
+struct {
+  TYPE type;
+  char name[50];
+} typedef OBJECT;
+
+struct {
+  OBJECT* elements;
+  int size;
+} typedef ARRAY;
+
+enum {
+      DEFINITON,
+      CONSTRUCTION,
+      ASSIGNMENT
+} typedef EXPRESSION_TYPE;
+  
+struct {
+  OBJECT destination;
+  ARRAY source;
+  OPERATOR operation;
+  EXPRESSION_TYPE type;
+} typedef EXPRESSION;
+
+struct {
+  ARRAY arguments;
+  EXPRESSION* expressions;
+  int expression_count;
+  TYPE return_type;
+} typedef FUNCTION;
+
--- src/main.c
+++ src/main.c
@@ -1,46 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-enum {
-      ASSIGN
-} typedef OPERATOR;
-
-enum {
-      INT,
-      FLOAT,
-      STRING,
-      CHAR
-} typedef TYPE;
-
-struct {
-  TYPE type;
-  char name[50];
-} typedef OBJECT;
-
-struct {
-  OBJECT* elements;
-  int size;
-} typedef ARRAY;
-
-enum {
-      DEFINITON,
-      CONSTRUCTION,
-      ASSIGNMENT
-} typedef EXPRESSION_TYPE;
-  
-struct {
-  OBJECT destination;
-  ARRAY source;
-  OPERATOR operation;
-  EXPRESSION_TYPE type;
-} typedef EXPRESSION;
-
-struct {
-  ARRAY arguments;
-  EXPRESSION* expressions;
-  int expression_count;
-  TYPE return_type;
-} typedef FUNCTION;
+#include "ast.h"
 
 FUNCTION MAIN;
 
